ca182be5407b1968e59c8d18a3f720c013f97c29
Yashwanth V <yashwanth.941v@gmail.com>
2026-07-29 00:25:31 +0530

    Initial Commit

---
+++ Typeboard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Typeboard
+//
+//  Created by Yashwanth V on 29/07/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Typeboard/TypeboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeboardApp.swift
+//  Typeboard
+//
+//  Created by Yashwanth V on 29/07/26.
+//
+
+import SwiftUI
+
+@main
+struct TypeboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
